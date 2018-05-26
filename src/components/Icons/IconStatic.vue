e0a6849ea3be96f7027453f4420c4467dc61fc45
Unknown <g_abriel10@hotmail.com>
2018-05-26 17:58:25 -0400

    Static icon setup

---
+++ src/components/Icons/IconStatic.vue
@@ -0,0 +1,34 @@
+<template>
+  <svg :alt="alt">
+    <use v-attr="attrObj" :href="path" />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    alt: {
+      type: String,
+      default: ''
+    },
+    svgId: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    path () {
+      return `/static/icons/icons.svg#${this.svgId}`;
+    },
+    attrObj () {
+      return {
+        'xlink:href': this.path,
+        'href': this.path
+      };
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+</style>
--- src/components/MainMenu/MainMenu.vue
+++ src/components/MainMenu/MainMenu.vue
@@ -1,39 +1,29 @@
 <template>
   <div>
     <router-link to="/settings" class="cog-animation">
-      <icon-base>
+      <icon-dynamic>
         <icon-cog />
-      </icon-base>
+      </icon-dynamic>
     </router-link>
     <ul>
       <li is="MainMenuItem" navigate-to="/">
-        <icon-base>
-          <icon-home />
-        </icon-base>
+        <icon-static svg-id="icon-home" alt="Home" />
         Home
       </li>
       <li is="MainMenuItem" navigate-to="/lists">
-        <icon-base>
-          <icon-numbered-list />
-        </icon-base>
+        <icon-static svg-id="icon-list-numbered" alt="Lists" />
         Lists
       </li>
       <li is="MainMenuItem" navigate-to="/pro">
-        <icon-base>
-          <icon-user />
-        </icon-base>
+        <icon-static svg-id="icon-user" alt="Pro" />
         Pro
       </li>
       <li is="MainMenuItem" navigate-to="/settings">
-        <icon-base>
-          <icon-cog />
-        </icon-base>
+        <icon-static svg-id="icon-cog" alt="Settings" />
         Settings
       </li>
       <li is="MainMenuItem" navigate-to="/about">
-        <icon-base>
-          <icon-info />
-        </icon-base>
+        <icon-static svg-id="icon-info" alt="About" />
         About
       </li>
     </ul>
@@ -42,22 +32,24 @@
 
 <script>
 import MainMenuItem from 'components/MainMenu/MainMenuItem';
-import IconBase from 'components/Icons/IconBase';
+import IconDynamic from 'components/Icons/IconDynamic';
 import IconCog from 'components/Icons/IconCog';
 import IconUser from 'components/Icons/IconUser';
 import IconInfo from 'components/Icons/IconInfo';
 import IconNumberedList from 'components/Icons/IconNumberedList';
 import IconHome from 'components/Icons/IconHome';
+import IconStatic from 'components/Icons/IconStatic';
 
 export default {
   components: {
     MainMenuItem,
-    IconBase,
+    IconDynamic,
     IconCog,
     IconUser,
     IconInfo,
     IconNumberedList,
-    IconHome
+    IconHome,
+    IconStatic
   },
   props: {}
 };
